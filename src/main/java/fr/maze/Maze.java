1159dc758420156e857e7ba222b698e2ad5e6e8c
Patrice CAVEZZAN <pcavezzan@gmail.com>
2023-05-11 14:37:54 +0200

    On crée le programme Maze pour pouvoir le tester.

---
+++ src/main/java/fr/maze/Maze.java
@@ -0,0 +1,104 @@
+package fr.maze;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Maze {
+
+    public void run() {
+        int rows = 7;
+        int columns = 7;
+        Cell[][] grid = new Cell[rows][columns];
+        Random r = new Random();
+
+        //initialize the maze
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                grid[row][column] = new Cell(row, column);
+            }
+        }
+
+        for (Cell[] gridRow : grid) {
+            for (Cell cell : gridRow) {
+                int row = cell.getRow();
+                int column = cell.getColumn();
+
+                cell.setNorth(getGridCell(row - 1, column, grid, rows, columns));
+                cell.setSouth(getGridCell(row + 1, column, grid, rows, columns));
+                cell.setWest(getGridCell(row, column - 1, grid, rows, columns));
+                cell.setEast(getGridCell(row, column + 1, grid, rows, columns));
+            }
+        }
+
+        //compute the maze : BinaryTree algorithm used here
+        for (Cell[] gridRow : grid) {
+            for (Cell cell : gridRow) {
+                List<Cell> neighbors = new ArrayList<Cell>();
+                if (cell.getNorth() != null) {
+                    neighbors.add(cell.getNorth());
+                }
+                if (cell.getEast() != null) {
+                    neighbors.add(cell.getEast());
+                }
+
+                Cell neighborCell = null;
+                if (neighbors.size() > 0) {
+                    int randomIndex = r.ints(1, 0, neighbors.size()).findFirst().getAsInt();
+                    neighborCell = neighbors.get(randomIndex);
+                }
+
+                if (neighborCell != null) {
+                    cell.getNeighbors().put(neighborCell, true);
+                    neighborCell.getNeighbors().put(cell, true);
+                }
+            }
+        }
+
+        //Display the maze
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("+");
+        for (int colCount = 0; colCount < columns; colCount++) {
+            sb.append("---+");
+        }
+        sb.append("\n");
+
+        for (Cell[] row : grid) {
+            StringBuffer top = new StringBuffer();
+            top.append("|");
+            StringBuffer bottom = new StringBuffer();
+            bottom.append("+");
+
+            for (Cell cell : row) {
+                cell = (cell == null ? new Cell(-1, -1) : cell);
+
+                boolean islinked = (cell.getNeighbors().get(cell.getEast()) != null ?
+                        cell.getNeighbors().get(cell.getEast()).booleanValue() : false);
+
+                String eastBoundary = (islinked ? " " : "|");
+                top.append("   ").append(eastBoundary);
+
+                islinked = (cell.getNeighbors().get(cell.getSouth()) != null ?
+                        cell.getNeighbors().get(cell.getSouth()).booleanValue() : false);
+
+                String southBoundary = (islinked ? "   " : "---");
+                bottom.append(southBoundary).append("+");
+            }
+
+            sb.append(top).append("\n");
+            sb.append(bottom).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static Cell getGridCell(int row, int column, Cell[][] grid, int rows, int columns) {
+        Cell resultCell = null;
+        if ((row >= 0 && row < rows) &&
+            (column >= 0 && (column < columns))) {
+            resultCell = grid[row][column];
+        }
+        return resultCell;
+    }
+}
